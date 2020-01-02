c306077ed0a770e3cf5e72a1485b4c67b2cba12c
sm-hwang <39686547+sm-hwang@users.noreply.github.com>
2020-01-01 17:23:43 -0700

    Removed methods/data for indexing
    Moving BST features for indexing to IndexableBST.py

--- CP3/BinarySearchTree.py
+++ CP3/BinarySearchTree.py
@@ -5,27 +5,26 @@ class Node:
     
     def __init__(self, key, left=None, right=None, data=None):
         self.key, self.left, self.right = key, left, right
-        self.parent, self.data, self.size = None, data, 1
+        self.parent, self.data = None, data
 
-
+        
 class BinarySearchTree:
 
     def __init__(self, initializer, key=lambda x: x):
         
         def build(left, right): 
             if left == right:
-                ret_node, size = Node(key(contents[left])), 1
+                ret_node= Node(key(contents[left]))
             elif left < right:
                 mid = (left + right) // 2
                 ret_node = Node(key(contents[mid]))
-                ret_node.left, left_size = build(left, mid - 1)
-                ret_node.right, right_size = build(mid + 1, right)
-                ret_node.size = size = 1 + left_count + right_count
-            return ret_node, size
+                ret_node.left = build(left, mid - 1)
+                ret_node.right = build(mid + 1, right)
+            return ret_node
         
         # O(nlogn) build time for a minimum height Binary Search Tree
         contents = sorted(initializer, key=key)
-        self.root, _ = build(0, len(contents) - 1)
+        self.root = build(0, len(contents) - 1)
 
     def search(k):
         node = self.root
@@ -93,40 +92,3 @@ class BinarySearchTree:
             self.transplant(node, y)
             y.left = node.left
             y.left.parent = y
-            
-    # Get (i + 1)th element of inorder traversal
-    # From Elements of Programming Interviews (Python)
-    def __getitem__(self, i):
-        i, node = i + 1, self.root
-        while node:
-            left_size = self._getSize(node.left)
-            if left_size + 1 < i:
-                i -= left_size + 1
-                node = node.right
-            elif left_size == i - 1:
-                break
-            else:
-                node = node.left
-        return node
-    
-    def _getSize(self, node):
-        return node.size if node else 0
-
-    # Should take as input "key" instead of node?
-    def index(self, node):
-        tree, count = self.root, 0
-        while tree and tree.key != node.key:
-            if node.key < tree.key:
-                tree = tree.left
-            else:
-                count += self._getSize(tree.left) + 1
-                tree = tree.right
-        # Will give nonsense answer if node.key not found in this tree
-        return self._getSize(tree.left) + count
-    
-    def index_node(self, node):
-        left_size, count = self._getSize(node.left), 0
-        while node.parent and node is node.parent.right: 
-            node = node.parent
-            count += self._getSize(node)
-        return left_size + count
